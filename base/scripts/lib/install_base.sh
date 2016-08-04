aaf7489cf9c81fe13bcc63d8e97275e22dd435ed
cspecter <chad.specter@gmail.com>
2016-08-04 17:43:18 -0400

    Update install_base.sh

--- base/scripts/lib/install_base.sh
+++ base/scripts/lib/install_base.sh
@@ -2,8 +2,4 @@
 set -e
 apt-get update -y
 apt-get install -y curl bzip2 build-essential python git
-# for npm module re-building
-apt-get -y install build-essential libssl-dev git python
-npm install -g node-gyp
-# pre-install node source code for faster building
-node-gyp install 4.4.7
+
