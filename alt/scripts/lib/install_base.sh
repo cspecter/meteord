fa8e0e82ef02c7fefdb3e1d1080409d7e20659c4
Chad Specter <chad.specter@gmail.com>
2016-08-05 17:11:33 -0400

    changed alt

--- alt/scripts/lib/install_base.sh
+++ alt/scripts/lib/install_base.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 apt-get update -y
-apt-get install -y curl bzip2 build-essential python git gcc g++
+apt-get upgrade -y
+apt-get install -y curl bzip2 build-essential python git
 
