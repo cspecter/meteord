5acf481aa44d151f5cdfd7d370afc375ace08f8f
cspecter <chad.specter@gmail.com>
2016-08-05 10:20:31 -0400

    Update build_app.sh

--- base/scripts/lib/build_app.sh
+++ base/scripts/lib/build_app.sh
@@ -7,7 +7,7 @@ BUNDLE_DIR=/tmp/bundle-dir
 # this fixes that
 cp -R /app $COPIED_APP_PATH
 cd $COPIED_APP_PATH
-
+meteor npm install --production
 meteor build --directory $BUNDLE_DIR --server=http://localhost:3000
 
 cd $BUNDLE_DIR/bundle/programs/server/
