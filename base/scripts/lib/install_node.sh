d78dc2a3029d90d4e1d3d97a1da388d87e146527
cspecter <chad.specter@gmail.com>
2016-08-05 11:46:47 -0400

    Update install_node.sh

--- base/scripts/lib/install_node.sh
+++ base/scripts/lib/install_node.sh
@@ -32,5 +32,6 @@ ln -sf /opt/nodejs/bin/npm /usr/bin/npm
 apt-get -y install build-essential libssl-dev git python
 npm config set python /usr/bin/python2.7
 npm install -g node-gyp
+apt-get install node-gyp
 # pre-install node source code for faster building
 node-gyp install 4.4.7
