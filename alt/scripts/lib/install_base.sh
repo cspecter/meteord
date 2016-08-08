59c6cff24e2fde1f68c0e9eaf09c06a84c3f5be4
Chad Specter <chad.specter@gmail.com>
2016-08-08 12:35:07 -0400

    changed alt

--- alt/scripts/lib/install_base.sh
+++ alt/scripts/lib/install_base.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
 apt-get update -y
-apt-get install -y curl bzip2 build-essential python git
+apt-get install -y curl bzip2 build-essential libssl-dev python git
+curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh
+bash install_nvm.sh
 
--- alt/scripts/lib/install_node.sh
+++ alt/scripts/lib/install_node.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
-apt-get update -y
-apt-get install -y build-essential libssl-dev
-curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh
-bash install_nvm.sh
+
 source ~/.profile
 nvm install 4.4.7
 npm i -g node-gyp
