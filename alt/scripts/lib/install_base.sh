d1ef003effb3d079762374688140d617d6234041
Chad Specter <chad.specter@gmail.com>
2016-08-09 12:43:35 -0400

    changed alt

--- alt/scripts/lib/install_base.sh
+++ alt/scripts/lib/install_base.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
 apt-get update -y
-apt-get install -y curl bzip2 build-essential libssl-dev python git
-#curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o install_nvm.sh
-#bash install_nvm.sh
-#source ~/.profile
+apt-get install -y curl bzip2 build-essential libssl-dev python git subversion
+
