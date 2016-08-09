ae71457dc4a98220a7e9b80c5b2c0cf7301b62b6
Chad Specter <chad.specter@gmail.com>
2016-08-09 10:50:46 -0400

    changed alt

--- alt/scripts/lib/install_node.sh
+++ alt/scripts/lib/install_node.sh
@@ -2,12 +2,14 @@
 set -e
 
 curl -sL https://deb.nodesource.com/setup_4.x | bash -
+apt-get update -y
 apt-get install -y nodejs
 
+npm -v
 npm cache clean -f
 npm install -g n
 n stable
 
 npm i -g node-gyp
 node-gyp install
-npm i talib
+npm i -g talib
