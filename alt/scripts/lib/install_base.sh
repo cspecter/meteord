b6a61afe5788e670ef7bd8f8156903965706d8ce
Chad Specter <chad.specter@gmail.com>
2016-11-28 14:21:38 -0500

    changed alt

--- alt/scripts/lib/install_base.sh
+++ alt/scripts/lib/install_base.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 apt-get update -y
-apt-get install -y curl bzip2 build-essential libssl-dev python git subversion aptitude libstdc++6 pkg-config libglib2.0-dev
+apt-get install -y curl bzip2 build-essential libssl-dev python git subversion aptitude libstdc++6 pkg-config libglib2.0-0
 
