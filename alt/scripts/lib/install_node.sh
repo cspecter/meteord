b0051f20a23a5a3dc7781f2292967eee2979997f
Chad Specter <chad.specter@gmail.com>
2016-11-29 11:42:37 -0500

    changed alt

--- alt/scripts/lib/install_node.sh
+++ alt/scripts/lib/install_node.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 set -e
 
-curl -sL https://deb.nodesource.com/setup_4.x | bash -
+curl -sL https://deb.nodesource.com/setup_6.x | bash -
 apt-get update -y
 apt-get install -y nodejs
-#apt-get install -y libavahi-compat-libdnssd-dev
 
 node -v
 npm -v
 
-#npm cache clean -f
-#npm install -g n
-#n stable
 
 npm -g install npm@latest
 npm i -g node-gyp
-node-gyp install 4.6.0
+#node-gyp install 4.6.0
 node-gyp -v
 npm install --global node-pre-gyp
-npm install --global nan got semver caw tar
+#npm install --global nan got semver caw tar
 export npm_config_loglevel=http; export npm_config_spin=false
-#export USER=root
-#export HOME=/tmp
-#sudo npm i -g talib
