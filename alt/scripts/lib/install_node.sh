b81e013c9c2da7804d5672d9ee4e0a1a7e09c8a5
Chad Specter <chad.specter@gmail.com>
2016-11-29 10:03:49 -0500

    changed alt

--- alt/scripts/lib/install_node.sh
+++ alt/scripts/lib/install_node.sh
@@ -18,7 +18,7 @@ npm i -g node-gyp
 node-gyp install 4.6.0
 node-gyp -v
 npm install --global node-pre-gyp
-npm install --global nan
+npm install --global nan got semver caw tar
 export npm_config_loglevel=http; export npm_config_spin=false
 #export USER=root
 #export HOME=/tmp
