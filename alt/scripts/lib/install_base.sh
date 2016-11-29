314d37137d9b53f0708cab49d1dc38ad0c071302
Chad Specter <chad.specter@gmail.com>
2016-11-29 10:41:19 -0500

    changed alt

--- alt/scripts/lib/install_base.sh
+++ alt/scripts/lib/install_base.sh
@@ -3,3 +3,4 @@ set -e
 apt-get update -y
 apt-get install -y curl bzip2 build-essential libssl-dev python git subversion aptitude libstdc++6 pkg-config libglib2.0-0 libvips-dev libavahi-compat-libdnssd-dev
 
+ldconfig
