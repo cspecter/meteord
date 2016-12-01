84035a2cb600de2523c90b28d227f29c6ef48ad3
Chad Specter <chad.specter@gmail.com>
2016-12-01 13:40:52 -0500

    added rebuild npm modules script

--- alt/scripts/lib/build_app.sh
+++ alt/scripts/lib/build_app.sh
@@ -33,8 +33,8 @@ node -p process.versions
 
 echo "=> Executing NPM install within Bundle"
 cd $BUNDLE_DIR/bundle/programs/server/
+rm -rf ./node_modules
 npm i
-meteor npm i
 
 echo "=> Moving bundle"
 mv $BUNDLE_DIR/bundle /built_app
