bb3c04a5478568a0e90d49117d21e964ed5c7a35
Chad Specter <chad.specter@gmail.com>
2016-12-13 12:30:31 -0500

    added timezone

--- alt/scripts/lib/install_base.sh
+++ alt/scripts/lib/install_base.sh
@@ -4,3 +4,7 @@ apt-get update -y
 apt-get install -y curl bzip2 build-essential libssl-dev python git subversion aptitude libstdc++6 pkg-config libglib2.0-0 libavahi-compat-libdnssd-dev libkrb5-dev
 
 ldconfig
+
+echo Europe/Stockholm >/etc/timezone && \
+ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime && \
+dpkg-reconfigure -f noninteractive tzdata
