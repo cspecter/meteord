5c8fe92fc02a1fdadda8cfece30c92320347c0be
Chad Specter <chad.specter@gmail.com>
2017-11-29 17:33:41 -0500

    change node version to 8

--- alt/scripts/lib/cleanup.sh
+++ alt/scripts/lib/cleanup.sh
@@ -8,7 +8,7 @@ apt-get autoremove -y
 rm -rf /usr/share/doc /usr/share/doc-base /usr/share/man /usr/share/locale /usr/share/zoneinfo
 
 # Clean out package management dirs
-rm -rf /var/lib/cache /var/lib/log
+# rm -rf /var/lib/cache /var/lib/log
 
 # Clean out /tmp
 rm -rf /tmp/*
