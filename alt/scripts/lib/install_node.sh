211aa00f6be7b8def4db4411612b163c2dc29fcf
Chad Specter <chad.specter@gmail.com>
2017-12-08 10:31:18 -0500

    change npm

--- alt/scripts/lib/install_node.sh
+++ alt/scripts/lib/install_node.sh
@@ -10,8 +10,8 @@ npm -v
 
 
 npm -g install npm@latest
-npm i -g node-gyp@latest
-npm explore npm -g -- npm install node-gyp
+npm i -g node-gyp@3.6.2
+npm explore npm -g -- npm install node-gyp@3.6.2
 node-gyp -v
-npm install --global node-pre-gyp
+npm install --global node-pre-gyp@0.6.39
 export npm_config_loglevel=http; export npm_config_spin=false
